091358de54dd6dc00f28228f42520b4209a3abb8
joshua-8 <59814881+joshua-8@users.noreply.github.com>
2021-05-09 22:16:02 -0700

    waiting for JMotor

---
+++ RCMv2.ino
@@ -0,0 +1,66 @@
+//   https://github.com/rcmgames/RCMv2
+#include <ESP32_easy_wifi_data.h>
+#include "rcm.h"
+
+void configWifi() { //see https://github.com/joshua-8/ESP32_easy_wifi_data/blob/master/examples/fullExample/fullExample.ino
+  EWD::routerName = " "; //name of the wifi network you want to connect to
+  EWD::routerPass = " "; //password for your wifi network (enter "-open-network-" if the network has no password) (default: -open-network-)
+  EWD::wifiPort = 25220; //what port the esp32 communicates on if connected to a wifi network (default: 25210)
+}
+
+void Enabled() { //code to run while enabled
+}
+
+void Enable() { //turn on outputs
+}
+
+void Disable() { //shut off all outputs
+}
+
+void PowerOn() { //runs once on robot startup, set pinmodes
+
+}
+
+void Always() { //always runs if void loop is running, don't control outputs here
+
+}
+
+void WifiDataToParse() {
+  enabled = EWD::recvBl();
+  //add data to read here: (EWD::recvBl, EWD::recvBy, EWD::recvIn, EWD::recvFl)(boolean, byte, int, float)
+
+}
+void WifiDataToSend() {
+  //EWD::sendFl(batteryVoltage);
+  //add data to send here:
+
+}
+
+void setup() {
+  PowerOn();
+  pinMode(ONBOARD_LED, OUTPUT);
+  Disable();
+  configWifi();
+  EWD::setupWifi(WifiDataToParse, WifiDataToSend);
+}
+
+void loop() {
+  EWD::runWifiCommunication();
+  if (!notTimedOut()) {
+    enabled = false;
+  }
+  Always();
+  if (enabled && !wasEnabled) {
+    Enable();
+  }
+  if (!enabled && wasEnabled) {
+    Disable();
+  }
+  if (enabled) {
+    Enabled();
+    digitalWrite(ONBOARD_LED, millis() % 500 < 250);
+  } else {
+    digitalWrite(ONBOARD_LED, HIGH);
+  }
+  wasEnabled = enabled;
+}
---
+++ rcm.h
@@ -0,0 +1,25 @@
+#define port1Pin 32
+#define port2Pin 33
+#define port3Pin 25
+#define port4Pin 26
+#define port5Pin 27
+//          PWM_CH, PIN
+#define port1 8,port1Pin
+#define port2 7,port2Pin
+#define port3 6,port3Pin
+#define port4 5,port4Pin
+#define port5 4,port5Pin
+//           PWM_CH, EN_PIN, PIN1, PIN2
+#define portA 3,23,22,19
+#define portB 1,5,18,21
+#define portC 2,17,16,12
+#define portD 0,13,4,14
+
+#define inport1 39
+#define inport2 34
+#define inport3 35
+
+#define ONBOARD_LED 2
+
+boolean enabled = false;
+boolean wasEnabled = false;
