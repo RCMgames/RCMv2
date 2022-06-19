e174d0da09d2f035103fa8036ae8b9e5522602fc
joshua-8 <joshua_phelps@brown.edu>
2022-06-19 16:45:13 -0400

    in progress, new lib versions + platformio

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -1,33 +1,66 @@
 //   https://github.com/rcmgames/RCMv2
 #include "rcm.h" //defines pins
-#include <ESP32_easy_wifi_data.h> //https://github.com/joshua-8/ESP32_easy_wifi_data
+#include <ESP32_easy_wifi_data.h> //https://github.com/joshua-8/ESP32_easy_wifi_data >=v1.0.0
 #include <JMotor.h> //https://github.com/joshua-8/JMotor
 
 // set up motors and anything else you need here
 
+float speed = 0;
+float turn = 0;
+
+float lSpeed = 0;
+float rSpeed = 0;
+
 const int dacUnitsPerVolt = 380;
 JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
 
+// https://github.com/joshua-8/JMotor/wiki/How-to-set-up-a-drivetrain
+JMotorDriverEsp32L293 leftDriver = JMotorDriverEsp32L293(portA); // left motor is connected to port A
+// motor_stop_voltage, motor_stop_speed, motor_start_voltage, motor_start_speed, motor_high_voltage, motor_high_speed, start_boost_time
+JMotorCompStandardConfig leftMotorConfig = JMotorCompStandardConfig(1.9, .553, 3.2, 1.24, 4.6, 1.89, 100);
+JMotorCompStandard leftMotorCompensator = JMotorCompStandard(voltageComp, leftMotorConfig, 100);
+JMotorControllerOpen lMotor = JMotorControllerOpen(leftDriver, leftMotorCompensator);
+
+JMotorDriverEsp32L293 rightDriver
+    = JMotorDriverEsp32L293(portD); // right motor is connected to port D
+// motor_stop_voltage, motor_stop_speed, motor_start_voltage, motor_start_speed, motor_high_voltage, motor_high_speed, start_boost_time
+JMotorCompStandardConfig rightMotorConfig = JMotorCompStandardConfig(1.9, .553, 3.2, 1.24, 4.6, 1.89, 100);
+JMotorCompStandard rightMotorCompensator = JMotorCompStandard(voltageComp, rightMotorConfig, 100);
+JMotorControllerOpen rMotor = JMotorControllerOpen(rightDriver, rightMotorCompensator);
+
+JDrivetrainTwoSide drivetrain = JDrivetrainTwoSide(lMotor, rMotor, 0.15); // width
+JDrivetrainControllerBasic drive = JDrivetrainControllerBasic(drivetrain, { INFINITY, INFINITY, INFINITY }, { .05, 0, 1 }, { 0, 0, 0 });
+
 void configWifi()
-{ // see https://github.com/joshua-8/ESP32_easy_wifi_data/blob/master/examples/fullExample/fullExample.ino
-    EWD::routerName = " "; // name of the wifi network you want to connect to
-    EWD::routerPass = " "; // password for your wifi network (enter "-open-network-" if the network has no password) (default: -open-network-)
-    EWD::wifiPort = 25220; // what port the esp32 communicates on if connected to a wifi network (default: 25210)
+{
+
+    EWD::mode = EWD::Mode::connectToNetwork;
+    EWD::routerName = "router";
+    EWD::routerPassword = "password";
+    EWD::routerPort = 25210;
+
+    // EWD::mode = EWD::Mode::createAP;
+    // EWD::APName = "rcm0";
+    // EWD::APPassword = "rcmpassword";
+    // EWD::APPort = 25210;
 }
 
 void Enabled()
 {
     // code to run while enabled
+    drive.moveVel({ speed * drive.getMaxVel().x, 0, turn * drive.getMaxVel().theta });
 }
 
 void Enable()
 {
     // turn on outputs
+    drive.enable();
 }
 
 void Disable()
 {
     // shut off all outputs
+    drive.disable();
 }
 
 void PowerOn()
@@ -38,17 +71,22 @@ void PowerOn()
 void Always()
 {
     // always runs if void loop is running, JMotor run() functions can be put here
+    drive.run();
+    delay(1);
 }
 
 void WifiDataToParse()
 {
     enabled = EWD::recvBl();
     // add data to read here: (EWD::recvBl, EWD::recvBy, EWD::recvIn, EWD::recvFl)(boolean, byte, int, float)
+    speed = EWD::recvFl();
+    EWD::recvFl();
+    turn = -EWD::recvFl(); // inverted to translate from standard rcmDS reference frame to JMotor/ROS standard
 }
 void WifiDataToSend()
 {
     EWD::sendFl(voltageComp.getSupplyVoltage());
-    // add data to send here:
+    // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
 }
 
 void setup()
@@ -64,7 +102,7 @@ void setup()
 void loop()
 { // don't edit
     EWD::runWifiCommunication();
-    if (EWD::timedOut()) {
+    if (!EWD::wifiConnected || EWD::timedOut()) {
         enabled = false;
     }
     Always();
@@ -76,9 +114,14 @@ void loop()
     }
     if (enabled) {
         Enabled();
-        digitalWrite(ONBOARD_LED, millis() % 500 < 250);
+        digitalWrite(ONBOARD_LED, millis() % 500 < 250); // flash, enabled
     } else {
-        digitalWrite(ONBOARD_LED, HIGH);
+        if (!EWD::wifiConnected)
+            digitalWrite(ONBOARD_LED, millis() % 1000 <= 100); // short flash, wifi connection fail
+        else if (EWD::timedOut())
+            digitalWrite(ONBOARD_LED, millis() % 1000 >= 100); // long flash, no driver station connected
+        else
+            digitalWrite(ONBOARD_LED, HIGH); // on, disabled
     }
     wasEnabled = enabled;
 }
