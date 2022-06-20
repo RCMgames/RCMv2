7261c046e642942d3c8dfbd4b7e8fe4e77c23030
joshua-8 <joshua_phelps@brown.edu>
2022-06-20 09:56:00 -0400

    comments, and rearranging

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -1,31 +1,18 @@
+//   This program is template code for programming small esp32 powered wifi controlled robots.
 //   https://github.com/rcmgames/RCMv2
+//   for information about the electronics, see the link at the top of this page: https://github.com/RCMgames
 #include "rcm.h" //defines pins
 #include <ESP32_easy_wifi_data.h> //https://github.com/joshua-8/ESP32_easy_wifi_data >=v1.0.0
 #include <JMotor.h> //https://github.com/joshua-8/JMotor
 
-// set up motors and anything else you need here
-const int dacUnitsPerVolt = 380;
+const int dacUnitsPerVolt = 380; // increasing this number decreases the calculated voltage
 JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
-
+// set up motors and anything else you need here
 // https://github.com/joshua-8/JMotor/wiki/How-to-set-up-a-drivetrain
-
-void configWifi()
-{
-
-    EWD::mode = EWD::Mode::connectToNetwork;
-    EWD::routerName = "router";
-    EWD::routerPassword = "password";
-    EWD::routerPort = 25210;
-
-    // EWD::mode = EWD::Mode::createAP;
-    // EWD::APName = "rcm0";
-    // EWD::APPassword = "rcmpassword";
-    // EWD::APPort = 25210;
-}
 
 void Enabled()
 {
-    // code to run while enabled
+    // code to run while enabled, put your main code here
 }
 
 void Enable()
@@ -40,13 +27,28 @@ void Disable()
 
 void PowerOn()
 {
-    // runs once on robot startup, set pin modes
+    // runs once on robot startup, set pin modes and use begin() if applicable here
 }
 
 void Always()
 {
-    // always runs if void loop is running, JMotor run() functions can be put here
+    // always runs if void loop is running, JMotor run() functions should be put here
+    // (but only the "top level", for example if you call drivetrainController.run() you shouldn't also call motorController.run())
+
     delay(1);
+}
+
+void configWifi()
+{
+    EWD::mode = EWD::Mode::connectToNetwork;
+    EWD::routerName = "router";
+    EWD::routerPassword = "password";
+    EWD::routerPort = 25210;
+
+    // EWD::mode = EWD::Mode::createAP;
+    // EWD::APName = "rcm0";
+    // EWD::APPassword = "rcmPassword";
+    // EWD::APPort = 25210;
 }
 
 void WifiDataToParse()
@@ -60,18 +62,20 @@ void WifiDataToSend()
     // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
 }
 
+////////////////////////////// you don't need to edit below this line ////////////////////
+
 void setup()
-{ // don't edit
+{
     Serial.begin(115200);
-    PowerOn();
     pinMode(ONBOARD_LED, OUTPUT);
+    PowerOn();
     Disable();
     configWifi();
     EWD::setupWifi(WifiDataToParse, WifiDataToSend);
 }
 
 void loop()
-{ // don't edit
+{
     EWD::runWifiCommunication();
     if (!EWD::wifiConnected || EWD::timedOut()) {
         enabled = false;
