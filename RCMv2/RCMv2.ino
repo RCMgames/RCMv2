05c63aeb069ed735d9b398cfa662830277a34952
joshua-8 <joshua_phelps@brown.edu>
2023-12-24 15:07:36 -0800

    blank template

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -6,7 +6,7 @@
 
 #include "rcm.h" //defines pins
 
-const int dacUnitsPerVolt = 380; // increasing this number decreases the calculated voltage
+const int dacUnitsPerVolt = 440; // increasing this number decreases the calculated voltage
 JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
 // set up motors and anything else you need here
 // https://github.com/joshua-8/JMotor/wiki/How-to-set-up-a-drivetrain
@@ -19,28 +19,14 @@ void Enabled()
 void Enable()
 {
     // turn on outputs
-    motorDriverA.enable();
-    motorDriverB.enable();
-    motorDriverC.enable();
-    motorDriverD.enable();
-    motorDriverE.enable();
-    motorDriverF.enable();
-    motorDriverG.enable();
-    motorDriverH.enable();
+
     digitalWrite(motorsEnablePin, HIGH);
 }
 
 void Disable()
 {
     // shut off all outputs
-    motorDriverA.disable();
-    motorDriverB.disable();
-    motorDriverC.disable();
-    motorDriverD.disable();
-    motorDriverE.disable();
-    motorDriverF.disable();
-    motorDriverG.disable();
-    motorDriverH.disable();
+
     digitalWrite(motorsEnablePin, LOW); // to be safe
 }
 
@@ -54,18 +40,6 @@ void Always()
     // always runs if void loop is running, JMotor run() functions should be put here
     // (but only the "top level", for example if you call drivetrainController.run() you shouldn't also call leftMotorController.run())
 
-    // turns on three motors at a time, cycling through all motors
-    int i = (millis() % 16000) / 2000;
-    RSLcolor = i == 0 ? CRGB(255, 255, 255) : CRGB(255, 0, 255);
-    motorDriverA.set((i != 0) * motorDriverA.getMaxRange());
-    motorDriverB.set((i != 1) * motorDriverB.getMaxRange());
-    motorDriverC.set((i != 2) * motorDriverC.getMaxRange());
-    motorDriverD.set((i != 3) * motorDriverD.getMaxRange());
-    motorDriverE.set((i != 4) * motorDriverE.getMaxRange());
-    motorDriverF.set((i != 5) * motorDriverF.getMaxRange());
-    motorDriverG.set((i != 6) * motorDriverG.getMaxRange());
-    motorDriverH.set((i != 7) * motorDriverH.getMaxRange());
-
     delay(1);
 }
 
@@ -78,7 +52,6 @@ void WifiDataToSend()
 {
     EWD::sendFl(voltageComp.getSupplyVoltage());
     // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
-    EWD::sendFl((millis() % 16000) / 2000);
 }
 
 void configWifi()
